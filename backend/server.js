a5da673631ac0e6eb5ec9bee51c10ed58b5b6bce
nirbarash88 <nirbarash88@gmail.com>
2024-12-17 12:38:50 +0200

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Backend is working!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
